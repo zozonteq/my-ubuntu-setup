9938e114598bf841ee1c6f1d6e7dffeb3a0d0def
zo <77091655+ZozonTeq@users.noreply.github.com>
2022-03-03 22:54:04 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#packages
+#install packages
 sudo apt update 
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt install git python python3 neovim vim gcc g++ nmap fish
+sudo apt install git python python3 neovim vim gcc g++ nmap fish hashcat default-jdk 
 
 
