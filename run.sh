d404759e4a9d81cca67dc36cafab14851aad2ef0
zo <77091655+ZozonTeq@users.noreply.github.com>
2022-03-16 08:48:13 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
-#install packages
+#setup packages
 sudo apt update 
+sudo apt upgrade
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt install git python python3 neovim vim gcc g++ nmap fish hashcat default-jdk 
 
+#Create test projects
+cd 
+mkdir repo && cd repo
+mkdir js-test cpp-test c-test python-test java-test
+cd js-test && touch main.js && cd ..
+cd cpp-test && touch main.cpp run.sh && "g++ main.cpp&&./a.out">>run.sh && cd ..
+cd c-test && touch main.c run.sh && "gcc main.c && ./a.out" >> run.sh && cd ..
+touch python-test/main.py
+touch java-test/main.java
 
